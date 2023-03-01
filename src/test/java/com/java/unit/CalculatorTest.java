aa35ef8e102dedb51a6372e3d8066c7908b8af86
alliano <allianoanonymous@gmail.com>
2023-03-01 12:00:38 +0900

    add methods testDivideSuccess and testDivideFail

--- src/test/java/com/java/unit/CalculatorTest.java
+++ src/test/java/com/java/unit/CalculatorTest.java
@@ -17,6 +17,26 @@ public class CalculatorTest {
 
         // menggunakan bantuan Class Assertions untuk membantu pengecekan, apakah variabel result nilai nya
         // sesuai dengan expetasi kita (20) atau tidak
-        Assertions.assertEquals(20, result);
+        Assertions.assertEquals(20, result); 
     }
+
+    @Test
+    public void testDivideSuccess() {
+
+        Integer result = calculator.divide(10, 10);
+
+        // expetasi kita disini 10 : 10 itu nga null
+        Assertions.assertNotNull(result);
+        // expetasi kita disini 10 : 10 itu hasil nya 1
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void testDivideFail() {
+        // expetasi ktia kalai ini calculator.devide(10, 0); harus melemparkan sebuah exception yang mana exception tersebuat adalah IllegalArgumentException
+        // disini harus terjadi sebuah exception jikalau tidak terjadi maka unit tes kita ini gagal
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {calculator.divide(10, 0);});
+    }
+
+
 }
