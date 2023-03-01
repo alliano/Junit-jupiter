251d1e58ba98b462a0f88a41bf05bf313adc8c7a
alliano <allianoanonymous@gmail.com>
2023-03-01 11:59:54 +0900

    add method divide to represent exceptions

--- src/main/java/com/java/unit/Calculator.java
+++ src/main/java/com/java/unit/Calculator.java
@@ -5,4 +5,13 @@ public class Calculator {
     public Integer add(Integer first, Integer second) {
         return first + second;
     }
+
+    public Integer divide(Integer first, Integer second){
+         if(second == 0){
+            throw new IllegalArgumentException("can not divide by zero");
+         }
+         else {
+            return first / second;
+         }
+    }
 }
