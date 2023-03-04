304266a68ae025d3c6fb5cfdbcdcd3ee8a4794e9
alliano <allianoanonymous@gmail.com>
2023-03-04 19:59:24 +0900

    example for extend class

---
+++ src/test/java/com/java/unit/RandomCalculatorExtendTest.java
@@ -0,0 +1,36 @@
+package com.java.unit;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+/**
+ * Kelas ini akan memwarisi atau memiliki apapun itu property ataupun method ataupun annotation
+ * yang ada pada parenc class nya, yaitu ParentCalculatorTest
+ */
+public class RandomCalculatorExtendTest extends ParentCalculatorTest {
+    
+    @Test
+    public void testAddCalculator(Random random, TestInfo testInfo) {
+        
+        int bilanganPertama = random.nextInt();
+
+        int bilanganKedua = random.nextInt();
+
+        int expected = (bilanganPertama+bilanganKedua);
+
+        Integer result = this.calculator.add(bilanganPertama, bilanganKedua);
+
+        Assertions.assertEquals(expected, result);
+
+        System.out.println(testInfo.getTestClass().get());
+    }
+
+    @AfterAll
+    public void tearDown() {
+        System.out.println("After All");
+    }
+}
